12cbb8068573f23057d88c63b4623a8f7756dd0e
jervenscabanilla <jervensc@gmail.com>
2023-10-28 00:26:25 +0800

    up nard (wala nabago)

--- Capstone_Group1/Assets/Scripts/RotateObject.cs
+++ Capstone_Group1/Assets/Scripts/RotateObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RotateObject : MonoBehaviour
 {
@@ -16,5 +17,6 @@ public class RotateObject : MonoBehaviour
     {
         particles.Play();
         Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
