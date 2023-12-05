92435b4b45c9a2cd30bde9801b469583dafad495
H3xaaa <jonardavenido2@gmail.com>
2023-12-05 22:32:24 +0800

    ang hirap

--- Capstone_Group1/Assets/Scripts/MainMenu.cs
+++ Capstone_Group1/Assets/Scripts/MainMenu.cs
@@ -5,27 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour 
 {
-	
-	FadeInOut fade;
-
-	void Start()
-	{
-		fade = FindObjectOfType<FadeInOut>();
-
-	}
-
-	public IEnumerator ChangeScene()
-	{
-		fade.FadeIn();
-		yield return new WaitForSeconds(1);
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-	}
-
-	public void PlayGame ()
-	{
-		StartCoroutine(ChangeScene());
-	}
-
 	public void QuitGame()
 	{
 		Debug.Log("QUIT");
